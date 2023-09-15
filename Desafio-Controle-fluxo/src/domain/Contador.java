627a7c2bb1f5ccf6f5d020e9fa600c702118ff1b
Perkles <wyllamispaiva@gmail.com>
2023-09-15 19:16:33 -0300

    update final do projeto

--- Desafio-Controle-fluxo/src/domain/Contador.java
+++ Desafio-Controle-fluxo/src/domain/Contador.java
@@ -1,31 +1,48 @@
 package domain;
 
+import exception.ParametrosInvalidosException;
+
 import java.util.Scanner;
 
 public class Contador {
-	public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("|=======Seja-bem vindo=======|");
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
 
-        try {
-            contar(parametroUm, parametroDois);
-        } catch (ParametrosInvalidosException exception) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        System.out.println("-------------------Desafio Controle de fluxo---------------------");
+        System.out.println("Digite dois valores para saber o número de interações entre eles.");
+        System.out.println("1º valor: ");
+        int paramentro1 = lerInteiro(input);
+
+        System.out.println("2º valor: ");
+        int paramentro2 = lerInteiro(input);
+
+        try{
+            contar(paramentro1, paramentro2);
+        }catch (ParametrosInvalidosException e){
+            System.out.println("Erro: " + e.getMessage());
         }
     }
 
-    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        if (parametroUm >= parametroDois) {
-            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
-        }
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 
-        int contagem = parametroDois - parametroUm;
-        for (int i = 1; i <= contagem; i++) {
-            System.out.println("Imprimindo o número " + i);
+            if (parametroUm >= parametroDois) {
+                throw new ParametrosInvalidosException();
+            }else {
+                int diferenca = parametroDois - parametroUm;
+                for (int i = 1; i <= diferenca; ++i) {
+                    System.out.println("Imprimindo a " + i + "ª interação: " + i);
+                }
+            }
+    }
+
+    public static int lerInteiro(Scanner scanner) {
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Digite um número inteiro:");
+            }
         }
     }
- }
+
+}
--- Desafio-Controle-fluxo/src/exception/ParametrosInvalidosException.java
+++ Desafio-Controle-fluxo/src/exception/ParametrosInvalidosException.java
@@ -1,7 +1,7 @@
 package exception;
 
 public class ParametrosInvalidosException extends Exception {
-	 public ParametrosInvalidosException(String message) {
-	        super(message);
-	    }
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro.");
+    }
 }
